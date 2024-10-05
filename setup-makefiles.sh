xaga-dev/android_device_xiaomi_miuicamera-xaga

Add vendor soong imports to extract
Change-Id: Ib19e69dc35af6b04519eeb695228af65f3d01d2a

setup-makefiles.sh
@@ -23,6 +23,12 @@ if [ ! -f "${HELPER}" ]; then
 fi
 source "${HELPER}"
 
+function vendor_imports() {
+    cat <<EOF >>"$1"
+		"vendor/xiaomi/miuicamera-xaga",
+EOF
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
 

frost-testzone/vendor_xiaomi_miuicamera-vili

setup-makefiles.sh
@@ -23,6 +23,12 @@ if [ ! -f "${HELPER}" ]; then
 fi
 source "${HELPER}"
 
+function vendor_imports() {
+    cat <<EOF >>"$1"
+		"vendor/xiaomi/miuicamera-lisa",
+EOF
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
 